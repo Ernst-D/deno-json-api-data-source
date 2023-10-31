575e723ba08848042c5246f7f041b3d9e8ee07e8
Ernst-D <ernst.dzerevanko@gmail.com>
2023-10-31 13:54:08 +0400

    new endpoint

--- server.ts
+++ server.ts
@@ -1,40 +1,20 @@
 import { Router } from "./router.ts";
+import { getJsonData } from "./transform.ts";
 
 const port = 8088;
 
 const defaultHandler = async () => new Response("Default response");
-
-const getUser = async (
-  req: Request,
-  params: Record<string, string>,
-): Promise<Response> => new Response("get user handler ");
-
-const addUser = async (
-  req: Request,
-  params: Record<string, string>,
-): Promise<Response> => new Response("add user handler ");
-
-const updateUser = async (
-  req: Request,
-  params: Record<string, string>,
-): Promise<Response> => new Response("update user handler");
-
-const addImage = async (
-  req: Request,
-  params: Record<string, string>,
-): Promise<Response> => new Response("add image handler");
+const jsonHandler = async () =>
+  new Response(JSON.stringify(await getJsonData()));
 
 const router = new Router();
 
 router.add("GET", "/", defaultHandler);
-router.add("GET", "/api/users/:userid", getUser);
-router.add("POST", "/api/users/:userid", updateUser);
-router.add("PUT", "/api/users/", addUser);
-router.add("GET", "/api/users/:userid/images/:imageid", addImage);
+router.add("GET", "/json", jsonHandler);
 
 async function handler(req: Request) {
   return await router.route(req);
 }
 
 console.log(`HTTP server running. Access it at: http://localhost:${port}/`);
-Deno.serve({ port }, handler);
+Deno.serve({ port, onError: () => new Response("Not found!") }, handler);
