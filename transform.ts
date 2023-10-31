045b5d89d794e934b059d7c4463d1bd4265b7471
Ernst-D <ernst.dzerevanko@gmail.com>
2023-10-31 13:53:27 +0400

    added transform

---
+++ transform.ts
@@ -0,0 +1,40 @@
+const homeDir = Deno.cwd();
+const pathToJsons = "/data";
+const fullPath = `${homeDir}${pathToJsons}`;
+
+export async function getJsonData() {
+  const files = [];
+
+  for await (const file of Deno.readDir(fullPath)) {
+    // console.log(file.name);
+    if(!file.name.endsWith(".json")){
+        return
+    }
+    const fsFile = await Deno.open(fullPath+"/"+file.name)
+    const fileStat = await Deno.fstat(fsFile.rid);
+
+    files.push({
+        file: file.name,
+        time: fileStat.mtime?.getTime()
+    });
+
+
+    // debugger
+    // // deno-lint-ignore ban-ts-comment
+    // // @ts-ignore
+    // return files.sort((a,b)=> a.time - b.time).map(file => file.name)
+    // .map((file) => fullPath + "/" + file);
+  }
+  
+
+
+    // deno-lint-ignore ban-ts-comment
+    // @ts-ignore
+    return files.sort((a,b)=> a.time - b.time)
+    .map((f) => fullPath + "/" + f.file);
+}
+
+debugger
+const data = await getJsonData();
+debugger
+console.log(data);
